133e9f34dac5c68d35c4f5d7455b91b1bd1051d3
jonathanXDD <jonathan27.tw@gmail.com>
2024-05-17 16:41:19 +0800

    5/17class

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Flowbite } from "flowbite-react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -12,6 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Flowbite>
+        {children}
+      </Flowbite>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 import Link from "next/link";
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
-import { Footer, FooterCopyright, FooterLink, FooterLinkGroup } from "flowbite-react";
+import { Footer, FooterCopyright, FooterLink, FooterLinkGroup ,DarkThemeToggle} from "flowbite-react";
 import { Carousel } from "flowbite-react";
 import { Card } from "flowbite-react";
 
@@ -10,25 +10,31 @@ import { Card } from "flowbite-react";
 export default function Home() {
   return (
     <>
-    <Navbar fluid rounded>
+    <div className="bg-[#93C5FD]">
+    <div class="container mx-auto">
+    <Navbar fluid rounded className="bg-white">
       <NavbarBrand as={Link} href="https://flowbite-react.com">
-        <img src="/next.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <img src="https://play-lh.googleusercontent.com/Fro4e_osoDhhrjgiZ_Y2C5FNXBMWvrb4rGpmkM1PDAcUPXeiAlPCq7NeaT4Q6NRUxRqo" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
         <NavbarLink href="#" active>
+          <span className="px4 py-2 hover:text-yellow-500 hover:border-yello-500 hover:border-b-2">
           Home
+          </span>
         </NavbarLink>
         <NavbarLink as={Link} href="#">
           About
         </NavbarLink>
-        <NavbarLink href="#">Services</NavbarLink>
-        <NavbarLink href="#">Pricing</NavbarLink>
-        <NavbarLink href="#">Contact</NavbarLink>
+        <NavbarLink href="#">景點</NavbarLink>
+        <NavbarLink href="#">資訊</NavbarLink>
+        <NavbarLink href="#">不要聯絡我</NavbarLink>
       </NavbarCollapse>
+      <DarkThemeToggle/>
     </Navbar>
-    <div className="h-70 sm:h-64 xl:h-80 2xl:h-96">
+    </div>
+    <div className="h-55 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
         <img src="https://vhx.imgix.net/criterionchannelchartersu/assets/ad353e5c-c570-482c-ab2b-9b7a13215602.jpg?auto=format%2Ccompress&fit=crop&h=720&q=75&w=1280" alt="..." />
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWq7qI0XyVN41egIS9AydJJ7YeckX5nBBaoqfqZnl-Cg&s" alt="..." />
@@ -54,6 +60,8 @@ export default function Home() {
       <FooterLink href="#">Contact</FooterLink>
     </FooterLinkGroup>
   </Footer>
+  
+</div>
   </>
   );
 }
